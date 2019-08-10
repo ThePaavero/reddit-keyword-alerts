75916c031d071b200f3ed5d2bc6680176bc70ff3
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-08-10 19:41:32 +0300

    Prince Andrew!

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 const axios = require('axios')
 const moment = require('moment')
+const colors = require('colors')
 const _ = require('lodash')
 const config = require('./config')
 
-const processSet = (set) => {
-  console.log(set)
-}
+const url = 'https://www.reddit.com/r/all.json'
 
 
 const init = (config) => {
-  config.sets.forEach(set => {
-    processSet(set)
-  })
+
+  axios.get(url)
+
+    .then(response => {
+
+      const items = response.data.data.children
+      const matches = []
+
+      config.sets.forEach(set => {
+        items.forEach(item => {
+          set.keywords.forEach(word => {
+            if (!item.data.title.includes(word)) {
+              return
+            }
+            matches.push(item.data)
+          })
+        })
+      })
+
+      console.log(matches)
+    })
+    .catch(console.log)
 }
 
 init(config)
